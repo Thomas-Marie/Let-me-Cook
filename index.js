5c000329da1825f3df7aa7a23a5a865c5e5f2169
Thomas Marie <thomus97@gmail.com>
2024-01-17 17:31:41 +0100

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('test request')
+});
+
+app.listen(port, () => {
+    console.log(`test exemple app listening at http://localhost:${port}`);
+});
