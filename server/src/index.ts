231c9db58f3c26a696527eb8b39a83ce315a5910
Thomas Marie <thomus97@gmail.com>
2024-02-22 16:52:00 +0100

    crud unit v1 finished

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRouter";
 import recipeRouter from "./routes/recipeRouter";
 import ingredientRouter from "./routes/ingredientRouter";
+import unitRouter from "./routes/unitRouter";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/recipe", recipeRouter);
 app.use("/ingredient", ingredientRouter);
+app.use("/unit", unitRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
